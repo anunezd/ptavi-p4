d1aa71b190336c5f51f1a67f23baf8f8ea26ebf5
anunezd <alejandro_nd93@hotmail.com>
2018-10-30 11:25:11 +0100

    Ejercicio 7 OK

--- client.py
+++ client.py
@@ -7,20 +7,26 @@ Programa cliente UDP que abre un socket a un servidor
 import sys
 import socket
 
+#Constantes, IP del servidor y contenido a enviar
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    LINEA = sys.argv[3]
+    REGISTERSIP = ''
+    USER = sys.argv[4]
+    EXPIRES = sys.argv[5]
+except IndexError:
+        sys.exit('Usage: client.py ip puerto register sip_address expires_val')
+REGISTERSIP = 'REGISTER sip:' + USER + ' SIP/2.0\r\n\r\n' + 'Expires:' + EXPIRES + '\r\n\r\n'
 
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
-METHOD = sys.argv[3]
-USER = sys.argv[4]
-REGISTERSIP = METHOD + ' sip:' + USER + ' SIP/2.0\r\n'
-
-#Creamos el socket, configuramos y atamos servidor-puerto
+#Creamos el socket, configuramos y atamos a servidor-puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     try:
         my_socket.connect((SERVER, PORT))
         print('Enviando: ' + REGISTERSIP)
-        my_socket.send(bytes(REGISTERSIP, 'utf-8'))
+        my_socket.send(bytes(REGISTERSIP, 'utf-8') + b'\r\n')
         data = my_socket.recv(1024)
         print('Recibido -- ', data.decode('utf-8'))
+
     except KeyboardInterrupt:
         print('Socket terminado.')
