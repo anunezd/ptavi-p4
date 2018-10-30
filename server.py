d284d8a8b364c1ebc0bd12b07b1e1633c54f6885
anunezd <alejandro_nd93@hotmail.com>
2018-10-30 13:09:42 +0100

    Ejercicio 11 OK

--- server.py
+++ server.py
@@ -26,16 +26,22 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             user = doc[1].split(':')[1]
             tiempo = datetime.now()
             expires = doc[2].split(':')[1].split('\r\n')[0]
-            expired = tiempo + timedelta(seconds=int(expires))
-            address = str(ip) + ":" + str(port)
-            fecha = expired.strftime(FORMATO)
-            self.dicc[user] = {'Address ' : address, 'Expires' : fecha}
-            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-            if expires == 0:
-                del self.dicc[user]
+            if int(expires) == 0:
+                try:
+                    del self.dicc[user]
+                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                except KeyError:
+                    pass
+            else:
+                expired = tiempo + timedelta(seconds=int(expires))
+                address = str(ip) + ":" + str(port)
+                fecha = expired.strftime(FORMATO)
+                self.dicc[user] = {'Address ' : address, 'Expires' : fecha}
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
             lista = []
+            now = datetime.now()
             for user in self.dicc:
-                if self.dicc[user]['Expires'] <= tiempo.strftime(FORMATO):
+                if self.dicc[user]['Expires'] <= now.strftime(FORMATO):
                     lista.append(user)
             for user in lista:
                 del self.dicc[user]
